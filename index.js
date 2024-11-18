f083a2c03d73f5d703f732cc0a7f910d301281da
Olha Fedchenko <ofedchen@gmail.com>
2024-11-18 18:25:58 +0100

    added hide/show

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let artStyle = ""
-// let userRequest = ""
-
 const input = document.querySelector("textarea");
 const log = document.getElementById("promptText");
-input.addEventListener("input", updateValue);
+const styleOnPage = document.createElement("h4")
+const wrapper = document.querySelector(".wrapper")
+const form = document.querySelector("form")
+const buttonDownload = document.getElementById("download")
+const buttonCreateNew = document.getElementById("create-new")
+const animatedH2 = document.getElementById("gen-animation")
 
-function updateValue(e) {
+input.addEventListener("input", onInput);
+
+function onInput(e) {
     log.textContent = e.target.value;
 }
 
@@ -19,13 +24,16 @@ function checkedValue(e) {
     console.log(artStyle)
 }
 
+function createRequest(style) {
+    let description = log.textContent
+    let uRequest = "Create an image according to this request: " + description + " in style:" + style
+    console.log(description)
+    console.log(uRequest)
+    return uRequest
+}
 
 function generateArt(uRequest) {
-
-    // let description = prompt("Describe a picture/visual art you would like to create")
-    // let artStyle = prompt("Write a style you would like your art to be")
-    // let userRequest = "Create an image according to this request: " + description + " in style: " + artStyle
-
+    console.log(uRequest)
     fetch('https://api.openai.com/v1/images/generations', {
         body: JSON.stringify({
             prompt: uRequest,
@@ -35,17 +43,20 @@ function generateArt(uRequest) {
             response_format: "url",
             style: "vivid"
         }),
-        headers: { 'Content-Type': 'application/json'
+        headers: {
+            'Content-Type': 'application/json'
         },
         method: 'POST'
     }).then(response => response.json()).then(result => {
         let link = result.data[0].url
         let userArt = document.querySelector("img")
         userArt.setAttribute("src", link)
-
         let download = document.querySelector("a")
         download.setAttribute("href", link)
 
+        animatedH2.style.display = "none"
+        buttonCreateNew.style.display = "block"
+        buttonDownload.style.display = "block"
     })
 }
 
@@ -53,16 +64,13 @@ document.getElementById("generate").addEventListener("click", (event) => {
     event.preventDefault();
     let userRequest = createRequest(artStyle)
     generateArt(userRequest)
+    form.style.display = "none"
+    log.style.display = "block"
+    animatedH2.style.display = "block"
+    styleOnPage.textContent = `Chosen style: ${artStyle}`
+    wrapper.insertBefore(styleOnPage, buttonCreateNew)
+    styleOnPage.style.display = "block"
 })
 
-document.getElementById("create-new").addEventListener("click", (event) => {
-    window.location.reload();
-})
+buttonCreateNew.addEventListener("click", () => { window.location.reload() })
 
-function createRequest(style) {
-    let description = log.textContent
-    let uRequest = "Create an image according to this request: " + description + " in style: " + style
-    console.log(description)
-    console.log(uRequest)
-    return uRequest
-}
